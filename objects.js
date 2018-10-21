f8d2b52b3c5ea20d5b12c5a7806529731a4aa17e
Patrick Hofmann <pm.hofmann@yahoo.co.uk>
2018-10-21 10:37:40 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,5 +1,5 @@
 var playlist = new Object({Eminem: "Without Me"});
 
-function updatePlaylist(artistName, songTitle){
+function updatePlaylist(playlist, artistName, songTitle){
   playlist.artistName = songTitle;
 }
